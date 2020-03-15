6189247068f89701c772568845bfaa20499af070
Natalia Banasiewicz <natalia.banasiewicz@wp.pl>
2020-03-15 18:34:24 +0100

    Add data for app and install shortid

---
+++ src/data/dataStore.js
@@ -0,0 +1,36 @@
+import shortid from 'shortid';
+
+/* content for PageTitle */
+export const titles = [
+  {
+    id: shortid.generate(), 
+    content: 'People',
+  },
+];
+
+/* initial content for cards and subcards */
+export const cards = [
+  {
+    id: shortid.generate(),
+    category: 'Age',
+    content: '40+',
+  },
+  {
+    id: shortid.generate(),
+    category: 'Ethnicity',
+    /* content for subcards */
+    content: ['Black', 'Hispanic'],
+  },
+  {
+    id: shortid.generate(),
+    category: 'Income',
+    content: 'yearly 15k USD +',
+  },
+];
+
+export const initialStoreData = {
+  cards: [...cards],
+  titles: [...titles],
+};
+
+export default initialStoreData;
