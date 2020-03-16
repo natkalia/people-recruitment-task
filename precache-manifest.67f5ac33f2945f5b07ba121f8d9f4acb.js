1518464519a9573b308979af30020d108c004e4b
Natalia Banasiewicz <natalia.banasiewicz@wp.pl>
2020-03-16 14:00:38 +0100

    Updates

---
+++ precache-manifest.67f5ac33f2945f5b07ba121f8d9f4acb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b95d210fa3f08626151eb15d5500fbda",
+    "url": "/people-recruitment-task/index.html"
+  },
+  {
+    "revision": "24e73ca163a2ecd20504",
+    "url": "/people-recruitment-task/static/css/main.c266ca0b.chunk.css"
+  },
+  {
+    "revision": "549d672ff4e4dc27f17a",
+    "url": "/people-recruitment-task/static/js/2.a51972b2.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/people-recruitment-task/static/js/2.a51972b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24e73ca163a2ecd20504",
+    "url": "/people-recruitment-task/static/js/main.c6a947fe.chunk.js"
+  },
+  {
+    "revision": "669c026626bf822ab0bc",
+    "url": "/people-recruitment-task/static/js/runtime-main.1361e070.js"
+  }
+]);
